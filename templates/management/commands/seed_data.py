aa0f8a44a256d9186b2bf069ca0ba8ad04bd7fd7
Abhishek Jagtap <159562929+Abhijagtp@users.noreply.github.com>
2025-05-16 12:36:40 +0530

    'admin_changes3'

--- templates/management/commands/seed_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from templates.models import Category
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        categories = ['React', 'HTML']
-        for name in categories:
-            Category.objects.get_or_create(name=name)
-        self.stdout.write(self.style.SUCCESS('Successfully seeded categories'))
